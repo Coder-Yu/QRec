883838d22892cab3357aa41628cbec9d1d97e1bb
Coder-Yu <yuyu2223@163.com>
2016-10-15 03:48:55 +0800

    initialization

---
+++ baseclass/recommender.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2016 School of Software Engineering, Chongqing University
+#
+# This file is part of RecQ.
+#
+# RecQ is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+class Recommender(object):
+    def __init__(self):
+        pass
+
+    def buildModel(self):
+        pass
+
+    def saveModel(self):
+        pass
+
+    def loadModel(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def computeCorr(self):
+        pass
+
+    def execute(self):
+        pass
---
+++ structure/sparseMatrix.py
@@ -0,0 +1,21 @@
+from scipy.sparse import csr_matrix
+import numpy as np
+
+
+class SparseMatrix():
+    'matrix used to store raw data'
+    def __init__(self,data,indices,indptr,shape=None):
+        self.matrix = csr_matrix((data,indices,indptr),shape)
+
+    def row(self,r):
+        return self.matrix.getrow(r).toarray()
+    def col(self,c):
+        return self.matrix.getcol(c).toarray().transpose()
+    def sRow(self,r):
+        'return the sparse row'
+        return self.matrix.getrow(r)
+    def sCol(self,c):
+        'return the sparse column'
+        return self.matrix.getcol(c)
+
+
