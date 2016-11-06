3ed2b9116987dd9230cb26e3976c50c7100352f9
Fang <fqq0429@gmail.com>
2016-11-06 18:32:54 +0800

    add SlopeOne

---
+++ algorithm/rating/SlopeOne.py
@@ -0,0 +1,14 @@
+from baseclass.recommender import Recommender
+class SlopeOne(Recommender):
+    def __init__(self,conf):
+        super(SlopeOne, self).__init__(conf)
+        pass
+
+    def initModel(self):
+        pass
+
+    def computeAverage(self):
+        pass
+
+    def predict(self,u,i):
+        pass
