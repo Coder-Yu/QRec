b860787547db542b288c1f9df02dc2e7cc880aab
Fang <fqq0429@gmail.com>
2016-11-13 18:18:41 +0800

    fix bugs

--- algorithm/rating/SlopeOne.py
+++ algorithm/rating/SlopeOne.py
@@ -19,7 +19,7 @@ class SlopeOne(Recommender):
                 new_x1,new_x2 = qmath.common(self.dao.testSet_i.keys()[i1],i2)
                 diff = new_x1 - new_x2
 
-                diffAverage_sub.setdefault(i2,diff.sum()/len(diff))
+                diffAverage_sub.setdefault(i2,diff.mean())
                 freq_sub.setdefault(i2,len(diff))
 
             self.diffAverage[i1] = diffAverage_sub
@@ -29,18 +29,18 @@ class SlopeOne(Recommender):
     def predict(self,u,i):
         itemDict = {}
         # check if the user existed in trainSet or not
-        if self.dao.containsUser(u) == True:
+        if self.dao.containsUser(u):
             for item in self.dao.row(u).valuse():
                 if item.values() > 0:
                     itemDict[item] = item.values()
                 else:
                     continue
-            Sum = 0
+            sum = 0
             freqSum = 0
             for item2 in itemDict.keys():
-                Sum = Sum + ((itemDict[item2] + self.diffAverage[u][item2]) * self.freq[u][item2])
+                sum = sum + ((itemDict[item2] + self.diffAverage[u][item2]) * self.freq[u][item2])
                 freqSum = freqSum + self.freq[u][item2]
-            pred = Sum/freqSum
+            pred = sum/freqSum
 
         else:
             pred = self.dao.itemMeans[u]
