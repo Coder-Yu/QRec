f3fbdef37a98ef1cab7163f01f00eaccb563771c
Coder-Yu <coderyy@outlook.com>
2016-11-24 17:43:31 +0800

    fixed method

--- algorithm/rating/BasicMF.py
+++ algorithm/rating/BasicMF.py
@@ -10,15 +10,19 @@ class BasicMF(IterativeRecommender):
             self.loss = 0
             for triple in self.dao.triple:
                 u,i,r = triple
-                u = self.dao.user[u]
-                i = self.dao.item[i]
-                error = r-self.P[u].dot(self.Q[i])
-                self.loss+=error**2
-                #update latent vectors
+                u = self.dao.getUserId(u)
+                i = self.dao.getItemId(i)
+                error = r - self.P[u].dot(self.Q[i])
+                self.loss += error**2
                 p = self.P[u].copy()
                 q = self.Q[i].copy()
+                self.loss += self.regU * p.dot(p) + self.regI * q.dot(q)
+
+                #update latent vectors
                 self.P[u] += self.lRate*(error*q-self.regU*p)
                 self.Q[i] += self.lRate*(error*p-self.regI*q)
+
+
             iteration += 1
             if self.isConverged(iteration):
                 break
