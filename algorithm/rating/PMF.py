bc4e912e615d1a91c01181a0527497a7285afca6
Coder-Yu <coderyy@outlook.com>
2016-11-27 02:12:52 +0800

    add PMF

--- algorithm/rating/PMF.py
+++ algorithm/rating/PMF.py
@@ -1,8 +1,8 @@
 from baseclass.IterativeRecommender import IterativeRecommender
 
-class BasicMF(IterativeRecommender):
+class PMF(IterativeRecommender):
     def __init__(self,conf):
-        super(BasicMF, self).__init__(conf)
+        super(PMF, self).__init__(conf)
 
     def buildModel(self):
         iteration = 0
@@ -16,10 +16,11 @@ class BasicMF(IterativeRecommender):
                 self.loss += error**2
                 p = self.P[u].copy()
                 q = self.Q[i].copy()
+                self.loss += self.regU * p.dot(p) + self.regI * q.dot(q)
 
                 #update latent vectors
-                self.P[u] += self.lRate*error*q
-                self.Q[i] += self.lRate*error*p
+                self.P[u] += self.lRate*(error*q-self.regU*p)
+                self.Q[i] += self.lRate*(error*p-self.regI*q)
 
 
             iteration += 1
