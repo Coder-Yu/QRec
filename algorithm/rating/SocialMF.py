bc3ec438e1b902a82bcca089c59a5131f9ecb024
DouTong <512537996@qq.com>
2016-12-03 11:49:30 +0800

    Update SocialMF.py

--- algorithm/rating/SocialMF.py
+++ algorithm/rating/SocialMF.py
@@ -24,31 +24,20 @@ class SocialMF(SocialRecommender ):
                 self.loss += error**2
                 p = self.P[u].copy()
                 q = self.Q[i].copy()
-                trustAffect = 0
+                relationError = 0
+                relationLoss = 0
                 for followee in followeesU:
-                    trustUV= followeesU[followee]
+                    weight= followeesU[followee]
                     uf = self.dao.getUserId(followee)
                     if uf <> -1 and self.dao.containsUser(uf):
-                        trustAffect += trustUV *self.P[uf]
-                relationLoss = p - trustAffect
-                self.loss += self.regU * p.dot(p) + self.regI * q.dot(q) + self.regB * (relationLoss .dot(relationLoss ))
+                        relationLoss += weight *self.P[uf]
+                relationError = p - relationLoss
+                self.loss += self.regU * p.dot(p) + self.regI * q.dot(q) + self.regS * (relationError .dot(relationError ))
 
 
                 # update latent vectors
-                indirect = 0
-                relationAffect = 0
-                for follower in followersU:
-                    trustVU = followersU[follower]
-                    uf2 = self.dao.getUserId(follower)
-                    followeesV = self.sao.getFollowees(uf2)
-                    for followee in followeesV:
-                        vf = self.dao.getUserId(followee)
-                        trustVW = followeesV[followee]
-                        if vf <> -1 and self.dao.containsUser(vf):
-                            indirect = trustVW *  self.P[vf]
-                    if uf2<> -1 and self.dao.containsUser(uf2):
-                        relationAffect = trustVU *(self.P[uf2] - indirect)
-                self.P[u] += self.lRate * (error * q - self.regU * p - self.regS * relationLoss+self.regS*relationAffect)
+               
+                self.P[u] += self.lRate * (error * q - self.regU * p - self.regS * relationError)
                 self.Q[i] += self.lRate * (error * p - self.regI * q)
 
 
