2dcc7bcb298222d0b463bba9373f5831cc14a54f
DOUTONG <512537996@qq.com>
2016-12-09 19:47:30 +0800

    socialMF finished

--- main/main.py
+++ main/main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     print '='*80
     print '0. Analyze the input data.(Configure the visual.conf in config/visual first.)'
     print '1. UserKNN   2. ItemKNN   3. BasicMF   4. SlopeOne   5. RSTE   6. UserMean'
-    print '7. ItemMean   8. SVD   9. PMF   10. TrustMF   11. SocialMF'
+    print '7. ItemMean   8. SVD   9. PMF   10. TrustMF   11. SocialMF   12. SoRec'
     algor = -1
     conf = -1
     print '-'*80
