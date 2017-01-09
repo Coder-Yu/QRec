c27f84d075bcca09e718b48b4c3f98ca7b025108
Yu <Coder-Yu@users.noreply.github.com>
2017-01-09 17:42:31 +0800

    Update README.md

--- main/main.py
+++ main/main.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
     print '='*80
     print '0. Analyze the input data.(Configure the visual.conf in config/visual first.)'
     print '1. UserKNN   2. ItemKNN   3. BasicMF   4. SlopeOne   5. RSTE   6. UserMean'
-    print '7. ItemMean   8. SVD   9. PMF   10. TrustMF   11. SocialMF   12. SoRec   13.SoReg   14.SVD++'
+    print '7. ItemMean   8. SVD   9. PMF   10. TrustMF   11. SocialMF   12. SoRec   13.SoReg'
+    print '14.SVD++   15.EE'
     algor = -1
     conf = -1
     print '-'*80
@@ -70,6 +71,9 @@ if __name__ == '__main__':
 
     elif order == 14:
         conf = Config('../config/SVDPlusPlus.conf')
+
+    elif order == 15:
+        conf = Config('../config/EE.conf')
 
 
     else:
