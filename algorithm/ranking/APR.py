5bbfec75c3725bef8ec0d4a7c57c3c020bdd2696
Yu <coderyy@outlook.com>
2018-10-09 13:34:28 +0800

    add APR

--- algorithm/ranking/APR.py
+++ algorithm/ranking/APR.py
@@ -1,8 +1,6 @@
 #coding:utf8
 from baseclass.DeepRecommender import DeepRecommender
-from random import choice,shuffle
-from tool.qmath import sigmoid
-from math import log
+
 import numpy as np
 import random
 from tool import config
@@ -17,8 +15,7 @@ set_random_seed(2)
 
 class APR(DeepRecommender):
 
-    # APR：Bayesian Personalized Ranking from Implicit Feedback
-    # Steffen Rendle,Christoph Freudenthaler,Zeno Gantner and Lars Schmidt-Thieme
+    # APR：Adversarial Personalized Ranking for Recommendation
 
     def __init__(self,conf,trainingSet=None,testSet=None,fold='[1]'):
         super(APR, self).__init__(conf,trainingSet,testSet,fold)
@@ -32,13 +29,17 @@ class APR(DeepRecommender):
         self.eps = float(args['-eps'])
         self.regAdv = float(args['-regA'])
         self.advEpoch = int(args['-advEpoch'])
+        self.negativeCount = 3
 
 
     def _create_variables(self):
+        #perturbation vectors
         self.adv_U = tf.Variable(tf.zeros(shape=[self.m, self.k]),dtype=tf.float32, trainable=False)
         self.adv_V = tf.Variable(tf.zeros(shape=[self.n, self.k]),dtype=tf.float32, trainable=False)
+
         self.neg_idx = tf.placeholder(tf.int32, [None], name="n_idx")
         self.V_neg_embed = tf.nn.embedding_lookup(self.V, self.neg_idx)
+        #parameters
         self.eps = tf.constant(self.eps,dtype=tf.float32)
         self.regAdv = tf.constant(self.regAdv,dtype=tf.float32)
 
@@ -56,6 +57,7 @@ class APR(DeepRecommender):
         return result
 
     def _create_adversarial(self):
+        #get gradients of Delta
         self.grad_U, self.grad_V = tf.gradients(self.loss_adv, [self.adv_U,self.adv_V])
 
         # convert the IndexedSlice Data to Dense Tensor
@@ -74,7 +76,7 @@ class APR(DeepRecommender):
                                tf.multiply(self.reg_lambda, tf.nn.l2_loss(self.V_embed)))
         self.reg_loss = tf.add(tf.multiply(self.reg_lambda, tf.nn.l2_loss(self.U_embed)), self.reg_loss)
         self.total_loss = tf.add(self.loss, self.reg_loss)
-
+        #loss of adversarial training
         self.loss_adv = tf.multiply(self.regAdv,tf.reduce_sum(tf.nn.softplus(-self._create_adv_inference())))
         self.loss_adv = tf.add(self.loss,self.loss_adv)
 
@@ -102,7 +104,7 @@ class APR(DeepRecommender):
         user_idx,item_idx=[],[]
         neg_item_idx = []
         for i,user in enumerate(users):
-            for j in range(3): #negative count
+            for j in range(self.negativeCount): #negative sampling
                 item_j = random.randint(0,self.n-1)
 
                 while self.dao.trainSet_u[user].has_key(self.dao.id2item[item_j]):
@@ -122,20 +124,21 @@ class APR(DeepRecommender):
             init = tf.global_variables_initializer()
             sess.run(init)
 
-            # 迭代，传递变量
+            # train the model until converged
             for epoch in range(self.maxIter):
-                # 按批优化
+
                 user_idx,item_idx,neg_item_idx = self.next_batch()
                 _,loss = sess.run([self.train,self.total_loss],feed_dict={self.u_idx: user_idx, self.v_idx: item_idx, self.neg_idx:neg_item_idx})
                 print 'iteration:', epoch, 'loss:',loss
 
-                # 输出训练完毕的矩阵
+
                 self.P = sess.run(self.U)
                 self.Q = sess.run(self.V)
                 self.ranking_performance()
 
+            # start adversarial training
             for epoch in range(self.advEpoch):
-                # 按批优化
+
                 user_idx,item_idx,neg_item_idx = self.next_batch()
                 sess.run([self.update_U, self.update_V],
                          feed_dict={self.u_idx: user_idx, self.v_idx: item_idx, self.neg_idx: neg_item_idx})
@@ -143,46 +146,10 @@ class APR(DeepRecommender):
 
                 print 'iteration:', epoch, 'loss:',loss
 
-                # 输出训练完毕的矩阵
                 self.P = sess.run(self.U)
                 self.Q = sess.run(self.V)
                 self.ranking_performance()
 
-    # def optimization(self,u,i,j):
-    #     s = sigmoid(self.P[u].dot(self.Q[i]) - self.P[u].dot(self.Q[j]))
-    #     self.P[u] += self.lRate * (1 - s) * (self.Q[i] - self.Q[j])
-    #     self.Q[i] += self.lRate * (1 - s) * self.P[u]
-    #     self.Q[j] -= self.lRate * (1 - s) * self.P[u]
-    #
-    #     self.P[u] -= self.lRate * self.regU * self.P[u]
-    #     self.Q[i] -= self.lRate * self.regI * self.Q[i]
-    #     self.Q[j] -= self.lRate * self.regI * self.Q[j]
-    #     self.loss += -log(s)
-    #
-    # def adv_update(self,u,i,j):
-    #     norm_u = l2((self.Q[i]- self.Q[j]))
-    #     norm_i = l2((self.P[u]))
-    #     self.adv_U[u] = self.eps*(self.Q[i]- self.Q[j])/norm_u
-    #     self.adv_V[i] = self.eps*(self.P[u])/norm_i
-    #     self.adv_V[j] = self.eps * (-self.P[u]) / norm_i
-    #
-    #     s = sigmoid((self.P[u]+self.adv_U[u]).dot(self.Q[i]+self.adv_V[i]) - (self.P[u]+self.adv_U[u]).dot(self.Q[j]+self.adv_V[j]))
-    #
-    #     self.P[u] += self.regAdv*self.lRate * (1 - s) * (self.Q[i] + self.adv_V[i] - self.Q[j] - self.adv_V[j])
-    #     self.Q[i] += self.regAdv*self.lRate * (1 - s) * (self.P[u] + self.adv_U[u])
-    #     self.Q[j] -= self.regAdv*self.lRate * (1 - s) * (self.P[u] + self.adv_U[u])
-    #
-    #     self.loss += -self.regAdv*log(s)
-
-    def predict(self,user,item):
-
-        if self.dao.containsUser(user) and self.dao.containsItem(item):
-            u = self.dao.getUserId(user)
-            i = self.dao.getItemId(item)
-            predictRating = sigmoid(self.Q[i].dot(self.P[u]))
-            return predictRating
-        else:
-            return sigmoid(self.dao.globalMean)
 
     def predictForRanking(self, u):
         'invoked to rank all the items for the user'
