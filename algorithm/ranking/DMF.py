69621ef017da16ea7537c516e9702e9fea9eb95c
Yu <coderyy@outlook.com>
2018-11-16 22:17:25 +0800

    refactor

--- algorithm/ranking/DMF.py
+++ algorithm/ranking/DMF.py
@@ -6,7 +6,7 @@ from tool import config
 try:
     import tensorflow as tf
 except ImportError:
-    print 'This method can only be run tensorflow!'
+    print 'This method can only run on tensorflow!'
     exit(-1)
 from tensorflow import set_random_seed
 set_random_seed(2)
