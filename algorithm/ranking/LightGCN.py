e8a4bf62f0934465d82e669121f24264cf17dfc1
yuyu2223 <47935533@qq.com>
2021-01-10 16:19:58 +1000

    MHCN is added

--- algorithm/ranking/LightGCN.py
+++ algorithm/ranking/LightGCN.py
@@ -21,7 +21,9 @@ class LightGCN(DeepRecommender):
         all_embeddings = [ego_embeddings]
         for k in range(self.n_layers):
             ego_embeddings = tf.sparse_tensor_dense_matmul(norm_adj,ego_embeddings)
-            all_embeddings += [ego_embeddings]
+            # normalize the distribution of embeddings.
+            norm_embeddings = tf.math.l2_normalize(ego_embeddings, axis=1)
+            all_embeddings += [norm_embeddings]
 
         all_embeddings = tf.reduce_sum(all_embeddings, axis=0)
 
