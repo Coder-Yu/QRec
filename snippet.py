8fc067de309c29f770fb516301bb0a61a4cba4c0
Coder-Yu <yuyu2223@163.com>
2021-07-31 00:46:11 +1000

    add snippet for direct use

--- snippet.py
+++ snippet.py
@@ -1,8 +1,14 @@
 #A snippet showing how to directly run algorithms in QRec
 from main.QRec import QRec #need to be modified according to your path
 from tool.config import Config #need to be modified according to your path
-#create your own config file
-#using absolute path for both config file and dataset files
-config = Config("/home/xxx/user.conf")
+#-----------------------------------------------------------------------------------
+#create your own config file by following the file format in the directory of config
+#-----------------------------------------------------------------------------------
+config = Config("/home/xxx/algor_name.conf")
 rec = QRec(config)
 rec.execute()
+#-----------------------------------------------------------------------------------
+
+#your own codes to use the recommendation results
+
+#-----------------------------------------------------------------------------------
