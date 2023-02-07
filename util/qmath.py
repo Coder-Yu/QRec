3f96abcdc75d483c9448a523a78cc6aa9f16ad7c
Junliang Yu <coderyy@outlook.com>
2023-02-08 08:57:16 +1000

    Update qmath.py

--- util/qmath.py
+++ util/qmath.py
@@ -131,33 +131,15 @@ def denormalize(vec,maxVal,minVal):
     return minVal+(vec-0.01)*(maxVal-minVal)
 
 @jit(nopython=True)
-def find_k_largest(K,candidates):
+def find_k_largest(K, candidates):
     n_candidates = []
-    for iid,score in enumerate(candidates[:K]):
-        n_candidates.append((iid, score))
-    n_candidates.sort(key=lambda d: d[1], reverse=True)
-    k_largest_scores = [item[1] for item in n_candidates]
-    ids = [item[0] for item in n_candidates]
-    # find the N biggest scores
-    for iid,score in enumerate(candidates[K:]):
-        ind = K
-        l = 0
-        r = K - 1
-        if k_largest_scores[r] < score:
-            while r >= l:
-                mid = int((r - l) / 2) + l
-                if k_largest_scores[mid] >= score:
-                    l = mid + 1
-                elif k_largest_scores[mid] < score:
-                    r = mid - 1
-                if r < l:
-                    ind = r
-                    break
-        # move the items backwards
-        if ind < K - 2:
-            k_largest_scores[ind + 2:] = k_largest_scores[ind + 1:-1]
-            ids[ind + 2:] = ids[ind + 1:-1]
-        if ind < K - 1:
-            k_largest_scores[ind + 1] = score
-            ids[ind + 1] = iid+K
-    return ids,k_largest_scores
+    for iid, score in enumerate(candidates[:K]):
+        n_candidates.append((score, iid))
+    heapq.heapify(n_candidates)
+    for iid, score in enumerate(candidates[K:]):
+        if score > n_candidates[0][0]:
+            heapq.heapreplace(n_candidates, (score, iid + K))
+    n_candidates.sort(key=lambda d: d[0], reverse=True)
+    ids = [item[1] for item in n_candidates]
+    k_largest_scores = [item[0] for item in n_candidates]
+    return ids, k_largest_scores
